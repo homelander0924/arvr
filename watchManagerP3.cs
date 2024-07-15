e91b0e793e7e985bab27b1e90836befbbc8e5118
homelander0924 <homelander0924@gmail.com>
2024-07-15 10:10:54 +0530

    Update watchManagerP3.cs

--- watchManagerP3.cs
+++ watchManagerP3.cs
@@ -1,59 +1,57 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class SwapWatch : MonoBehaviour
-{
-public GameObject[] Watchwindow;
-public GameObject[] WatchModel;
-Animation anim1,anim2;
+
+public class SwapWatch : MonoBehaviour {
+  public GameObject[] Watchwindow;
+  public GameObject[] WatchModel;
+  Animation anim1,anim2;
 // Start is called before the first frame update
-void Start()
-{
-WatchModel[0].SetActive(true);
-Watchwindow[0].SetActive(true);
-WatchModel[1].SetActive(false);
-Watchwindow[1].SetActive(false);
-//get component of animation
-anim1=Watchwindow[0].GetComponent<Animation>();
-anim2=Watchwindow[1].GetComponent<Animation>();
-}
-public void Watch1()
-{
-WatchModel[0].SetActive(true);
-Watchwindow[0].SetActive(true);
-WatchModel[1].SetActive(false);
-Watchwindow[1].SetActive(false);
-anim1["watch1Anim"].speed=1.0f;
-anim1.Play();
-
-}
-public void watch2()
-{
-WatchModel[0].SetActive(false);
-Watchwindow[0].SetActive(false);
-WatchModel[1].SetActive(true);
-Watchwindow[1].SetActive(true);
-anim2["watch2Anim"].speed=1.0f;
-anim2.Play();
-}
-public void buttonCloseClick()
-{
-string
-buttonName=EventSystem.current.currentSelectedGameObject.name;
-if(buttonName=="close1")
-{
-//reverse of close animation
-anim1["watch1Anim"].speed=-1.0f;
-anim1["watch1Anim"].time=
-anim1["watch1Anim"].length;
-anim1.Play();
-}
-else if(buttonName=="close2")
-{
-anim2["watch2Anim"].speed=-1.0f;
-anim2["watch2Anim"].time=
-anim2["watch2Anim"].length;
-anim2.Play();
-}
-}
-
+  void Start() {
+    WatchModel[0].SetActive(true);
+    Watchwindow[0].SetActive(true);
+    WatchModel[1].SetActive(false);
+    Watchwindow[1].SetActive(false);
+    //get component of animation
+    anim1=Watchwindow[0].GetComponent<Animation>();
+    anim2=Watchwindow[1].GetComponent<Animation>();
+  }
+  
+  public void Watch1() {
+    WatchModel[0].SetActive(true);
+    Watchwindow[0].SetActive(true);
+    WatchModel[1].SetActive(false);
+    Watchwindow[1].SetActive(false);
+    anim1["watch1Anim"].speed=1.0f;
+    anim1.Play();
+  }
+  
+  public void watch2() {
+    WatchModel[0].SetActive(false);
+    Watchwindow[0].SetActive(false);
+    WatchModel[1].SetActive(true);
+    Watchwindow[1].SetActive(true);
+    anim2["watch2Anim"].speed=1.0f;
+    anim2.Play();
+  }
+  
+  public void buttonCloseClick() {
+  string
+  buttonName=EventSystem.current.currentSelectedGameObject.name;
+  if(buttonName=="close1")
+  {
+    //reverse of close animation
+    anim1["watch1Anim"].speed=-1.0f;
+    anim1["watch1Anim"].time=
+    anim1["watch1Anim"].length;
+    anim1.Play();
+  }
+  else if(buttonName=="close2")
+  {
+    anim2["watch2Anim"].speed=-1.0f;
+    anim2["watch2Anim"].time=
+    anim2["watch2Anim"].length;
+    anim2.Play();
+  }
+  }
+  
 }
